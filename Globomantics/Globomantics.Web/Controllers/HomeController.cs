e4c0e13af23dbc96fdf8da8810cc29093c15ef11
Davorin <davorin.jurkovic@gmail.com>
2025-03-02 00:16:15 +0100

    Routing -- Validation for productId

--- Globomantics/Globomantics.Web/Controllers/HomeController.cs
+++ Globomantics/Globomantics.Web/Controllers/HomeController.cs
@@ -32,13 +32,19 @@ public class HomeController : Controller
     /// This was done with Alternative route to avoid attribute on the TicketDetails action, in Program.cs
     /// Method signature must be then
     /// public IActionResult TicketDetails(Guid productId, string? slug)
+    /// 
+    /// When defining the route, the optional parameters are simply indicated by adding the question mark.
+    /// Make sure that your parameter that you're expecting in your action is also marked with a question mark to
+    /// indicate that it's a nullable type.
+    /// That way the nullable reference types in .NET and C# will properly notify you if there are potential null
+    /// reference exception.
     /// </summary>
-    /// <param name="productId"></param>
+    /// <param name="productId">We define that our productId has to be Guid</param>
     /// <param name="slug"></param>
     /// <returns></returns>
     /// <exception cref="NotImplementedException"></exception>
-    [Route("/details/{productId}/{slug}")]
-    public IActionResult TicketDetails(Guid productId, string slug)
+    [Route("/details/{productId:guid}/{slug?}")]
+    public IActionResult TicketDetails(Guid productId, string? slug)
     {
         var product = productRepository.Get(productId);
 
