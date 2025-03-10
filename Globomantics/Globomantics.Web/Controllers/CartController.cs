b70ad1b016d6d40d9696330bf7d539eedfe746eb
Davorin <davorin.jurkovic@gmail.com>
2025-03-10 23:19:46 +0100

    Working with Models and Data -- Model Binding in ASP.NET Core MVC -- Model Binding and HttpPost -- Validation

--- Globomantics/Globomantics.Web/Controllers/CartController.cs
+++ Globomantics/Globomantics.Web/Controllers/CartController.cs
@@ -1,19 +1,57 @@
-﻿using Globomantics.Web.Models;
+﻿using Globomantics.Domain.Models;
+using Globomantics.Infrastructure.Repositories;
+using Globomantics.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Globomantics.Web.Controllers
 {
+    [Route("[controller]")]
     public class CartController : Controller
     {
+        private readonly ILogger<CartController> logger;
+        private readonly ICartRepository cartRepository;
+        private readonly IRepository<Customer> customerRepository;
+        private readonly IRepository<Order> orderRepository;
+        //private readonly IStateRepository stateRepository;
+
+        public CartController(ICartRepository cartRepository,
+            IRepository<Customer> customerRepository,
+            IRepository<Order> orderRepository,
+            //IStateRepository stateRepository,
+            ILogger<CartController> logger)
+        {
+            this.logger = logger;
+            this.cartRepository = cartRepository;
+            this.customerRepository = customerRepository;
+            this.orderRepository = orderRepository;
+            //this.stateRepository = stateRepository;
+        }
+
         public IActionResult Index(Guid? id)
         {
             return View();
         }
 
         [HttpPost]
+        [Route("Add")]
         public IActionResult AddToCart(AddToCartModel addToCartModel)
         {
-            throw new NotImplementedException();
+            if(addToCartModel.Product is null)
+            {
+                return BadRequest();
+            }
+
+            logger.LogInformation($"Adding products " +
+                $"{addToCartModel.Product.ProductId} to cart " +
+                $"{addToCartModel.CartId}");
+
+            var cart = cartRepository.CreateOrUpdate(addToCartModel.CartId,
+                            addToCartModel.Product.ProductId,
+                            addToCartModel.Product.Quantity);
+
+            cartRepository.SaveChanges();
+
+            return RedirectToAction("Index", "Cart");
         }
 
         [HttpPost]
@@ -23,11 +61,85 @@ namespace Globomantics.Web.Controllers
         }
 
         [HttpPost]
+        [Route("Finalize")]
         public IActionResult Create(CreateOrderModel createOrderModel)
         {
-            throw new NotImplementedException();
+            if (createOrderModel.Customer is null)
+            {
+                ModelState.AddModelError("Customer",
+                    "Customer data is not set correctly");
+
+                return View("Index");
+            }
+
+            if (createOrderModel.Customer.Name.Length <= 2)
+            {
+                ModelState.AddModelError(nameof(createOrderModel.Customer.Name),
+                    "Too short name.");
+
+                return View("Index");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View("Index");
+            }
+
+            var customer = new Customer
+            {
+                Email = createOrderModel.Customer.Email,
+                Name = createOrderModel.Customer.Name,
+                City = createOrderModel.Customer.City,
+                Country = createOrderModel.Customer.Country,
+                ShippingAddress = createOrderModel.Customer.ShippingAddress,
+                PostalCode = createOrderModel.Customer.PostalCode,
+            };
+
+            logger.LogInformation($"Creating a new order for {customer.CustomerId}");
+
+            customerRepository.Add(customer);
+
+            var order = new Order
+            {
+                CustomerId = customer.CustomerId
+            };
+
+            if (createOrderModel.CartId is null || createOrderModel.CartId == Guid.Empty)
+            {
+                ModelState.AddModelError("Cart", "Cart has been deleted");
+
+                return View("Index");
+            }
+
+            var cart = cartRepository.Get(createOrderModel.CartId.Value);
+
+            if (cart is null)
+            {
+                ModelState.AddModelError("Cart", "Cart has been deleted");
+
+                return View("Index");
+            }
+
+            foreach (var lineItem in cart.LineItems)
+            {
+                order.LineItems.Add(lineItem);
+            }
+
+            orderRepository.Add(order);
+
+            cartRepository.Update(cart);
+
+            cartRepository.SaveChanges();
+
+            logger.LogInformation($"Order placed for {customer.CustomerId}");
+
+            //stateRepository.Remove("NumberOfItems");
+            //stateRepository.Remove("CartId");
+
+            return RedirectToAction("ThankYou");
         }
 
+        [Route("ThankYou")]
         public IActionResult ThankYou()
         {
             return View();
